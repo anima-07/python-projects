d549ef214884f3063c090763180674ce9e81ac03
anima-07 <59792492+anima-07@users.noreply.github.com>
2020-01-12 03:48:22 -0700

    Add files via upload

---
+++ IDK.py
@@ -0,0 +1,19 @@
+name_list = ["Evan", "Cody", "Kristan",  "Kristine"]
+
+while True:
+    print("Hello, My name is Travis..")
+    name = input("Please enter your username").strip().capitalize()
+
+    if name in name_list:
+        print("Hello [!]".format(name)
+        remove = ('Would you like to be removed from the database? (y/n): ').lower()
+
+
+        if remove == "y"
+            print(name_list)  
+            name_list.remove(name)
+            print(name_list)  
+
+    else:
+            print("access denied")
+    
